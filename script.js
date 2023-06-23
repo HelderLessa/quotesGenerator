b4133610ebfb540125a7fbcaaecbdb838dc4b8ff
Helder Lessa <116907887+HelderLessa@users.noreply.github.com>
2023-06-23 10:48:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,16 @@
-"use strict";
+const quotesDiv = document.getElementById("quotes");
+const authorDiv = document.querySelector(".author");
 
-const quoteContainer = document.getElementById("quote-container");
-const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("author");
-const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
-const loader = document.getElementById("loader");
-
-let apiQuotes = [];
-
-function showLoadingSpinner() {
-  loader.hidden = false;
-  quoteContainer.hidden = true;
-}
-
-function removeLoadingSpinner() {
-  loader.hidden = true;
-  quoteContainer.hidden = false;
-}
-
-// Show New Quote
-function newQuote() {
-  showLoadingSpinner();
-  // Pick a random quote from apiQuotes array
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  if (!quote.author) authorText.textContent = "Unknown";
-  authorText.textContent = quote.author;
-
-  quote.text.length > 120
-    ? quoteText.classList.add("long-quote")
-    : quoteText.classList.remove("long-quote");
-
-  // Set Quote, Hide Loader
-  quoteText.textContent = quote.text;
-  removeLoadingSpinner();
-}
-
-// Get Quotes From API
-async function getQuotes() {
-  showLoadingSpinner();
-  const apiUrl = "https://type.fit/api/quotes";
+async function generateQuote() {
   try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
+    const response = await fetch("https://api.quotable.io/random");
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+    const data = await response.json();
+    quotesDiv.textContent = `"${data.content}"`;
+    authorDiv.textContent = `- ${data.author}`;
   } catch (err) {
-    console.error(err);
+    alert(err.message);
   }
 }
-
-// Tweet Quote
-function tweetQuote() {
-  const twitterUrl = `https://twitter.com/intent/tweet?text${quoteText.textContent} - ${authorText.textContent}`;
-  window.open(twitterUrl, "_blank");
-}
-
-// Event listenets
-newQuoteBtn.addEventListener("click", newQuote);
-twitterBtn.addEventListener("click", tweetQuote);
-
-// On Load
-getQuotes();
